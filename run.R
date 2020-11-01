eae367bb81b083bb75f6f51eb95c674c700b698f
slwu89 <slwu89@berkeley.edu>
2020-11-01 15:09:52 -0800

    clean up plot

--- run.R
+++ run.R
@@ -28,9 +28,10 @@ out <- pfsim_aggregated(
   verbose = T
 )
 
-out <- data.table::as.data.table(discretise(out = out,dt = 1))
+out <- data.table::as.data.table(stocheulerABM::discretise(out = out,dt = 1))
 out <- data.table::melt(out,id.vars = "time",measure.vars = c("SH","IH","SV","IV"))
 out[, ("mean") := cumsum(value)/1:.N, by = .(variable)]
+out[, ("species") := ifelse(variable %in% c("SH","IH"),"Human","Mosquito")]
 
 # ggplot(data = out) +
 #   geom_line(aes(x=time,y=value,color=variable)) +
@@ -40,10 +41,12 @@ dde_out <- dede(y = IC$y0,times = 0:tmax,func = derivs,parms = IC$parameters)
 
 dde_out <- as.data.table(dde_out)
 dde_out <- data.table::melt(dde_out,id.vars = "time",measure.vars = c("SH","IH","SV","IV"))
+dde_out[, ("species") := ifelse(variable %in% c("SH","IH"),"Human","Mosquito")]
 
 ggplot(data = out) +
   geom_line(aes(x=time,y=value,color=variable),alpha=0.75) +
   geom_line(aes(x=time,y=value,color=variable),alpha=0.75,data=dde_out,linetype=2) +
   geom_line(aes(x=time,y=mean,color=variable)) +
+  facet_wrap(. ~ species,scales = "free_y") +
   theme_bw()
 
