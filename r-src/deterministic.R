f490087c12db7cd1ecc47788df69d308049292f4
slwu89 <slwu89@berkeley.edu>
2020-11-01 15:05:30 -0800

    aggregated simulation code works

---
+++ r-src/deterministic.R
@@ -0,0 +1,87 @@
+
+# NH = size of human pop
+# X = prevalence in humans
+# g = 1/avg lifespan
+# a = human bloodfeeding rate
+# b = mosy -> human transmission efficiency
+# c = human -> mosy transmission efficiency
+# r = 1/avg duration of infection in humans
+# EIP = length of EIP
+# LEP = duration in liver
+calc_equilibrium <- function(NH,X,g=1/12,a=(0.9 * 1/3),b=0.55,c=0.15,r=1/200,EIP=10,LEP=7){
+
+  P <- exp(-g*EIP)
+  SH <- NH*(1-X)
+  IH <- NH-SH
+
+  # equilibrium IV
+  IV <- (r*IH*(SH + IH)) / (a * b * SH)
+
+  # prob mosquito becomes infected and survives to become infectious
+  p_Minf <- (a*c*X) / ((a*c*X) + g)
+  p_Minf <- p_Minf * P
+
+  # total num mosy
+  NV <- IV / p_Minf
+
+  # solve for lambda
+  lambda <- NV*g
+
+  # calc EV
+  EV <- (1/g) * ( ((g*IV)/P)   - (g*IV) )
+
+  # susceptible vectors
+  SV <- NV - IV - EV
+
+  list(
+    "y0" = c(SH=SH,IH=IH,SV=SV,IV=IV),
+    "parameters" = c(g=g,b=b,a=a,c=c,r=r,EIP=EIP,LEP=LEP,SV0=SV,IV0=IV,SH0=SH,IH0=IH,lambda=lambda)
+  )
+}
+
+# for deSolve::dede
+derivs <- function(t,y,pars) {
+
+  with(as.list(pars),{
+    SH <- y[1]
+    IH <- y[2]
+    SV <- y[3]
+    IV <- y[4]
+
+    if(t <= EIP){
+      SV_eip <- SV0
+
+      SH_eip <- SH0
+      IH_eip <- IH0
+    } else {
+      SV_eip <- lagvalue(t - EIP,3)
+
+      SH_eip <- lagvalue(t - EIP,1)
+      IH_eip <- lagvalue(t - EIP,2)
+    }
+
+    if(t <= LEP){
+      SH_lep <- SH0
+      IH_lep <- IH0
+
+      IV_lep <- IV0
+    } else {
+      SH_lep <- lagvalue(t - LEP,1)
+      IH_lep <- lagvalue(t - LEP,2)
+
+      IV_lep <- lagvalue(t - LEP,4)
+    }
+
+    X_lep <- IH_lep / (SH_lep + IH_lep)
+    X_eip <- IH_eip / (SH_eip + IH_eip)
+    X <- IH / (SH + IH)
+
+    dSH <- (r * IH) - (a * b * (1 - X) * IV)
+    dIH <- (a * b * IV_lep * (1 - X_lep)) - (r * IH)
+
+    dSV <- lambda - (a * c * X * SV) - (g * SV)
+    dIV <- (a * c * X_eip * SV_eip * exp(-g * EIP)) - (g * IV)
+
+    return(list(c(dSH,dIH,dSV,dIV)))
+  })
+}
--- run.R
+++ run.R
@@ -1,174 +1,49 @@
+rm(list=ls());gc()
+
 library(here)
 library(data.table)
 library(ggplot2)
+library(deSolve)
 library(Rcpp)
 library(stocheulerABM)
 
 Rcpp::sourceCpp(here::here("r-src/aggregated.cpp"))
+source(here::here("r-src/deterministic.R"))
 
-params <- c("lambda" = 5, "g" = 1/12, "b" = 0.55, "c" = 0.15, "a" = 0.9 * 1/3, "r" = 1/200, "EIP" = 5, "LEP" = 7)
+NH <- 1e3
+X <- 0.3
+
+IC <- calc_equilibrium(NH = NH,X = X)
+
+
 tmax <- 365*50
-
-# out <- pfsim_aggregated(
-#   tmax = tmax,
-#   SH = 500,
-#   IH = 10,
-#   SV = 100,
-#   IV = 5,
-#   parameters = params,
-#   verbose = T
-# )
-# 
-# out <- data.table::as.data.table(discretise(out = out,dt = 1))
-# out <- data.table::melt(out,id.vars = "time",measure.vars = c("SH","IH","SV","IV"))
-# 
-# ggplot(data = out) +
-#   geom_line(aes(x=time,y=value,color=variable)) +
-#   theme_bw()
-
-
-
-
-
-SH0 <- 100
-IH0 <- 50
-
-
-calc_equilibrium <- function(parameters, SH, IH){
-  with(as.list(parameters),{
-    X <- (IH / (IH + SH))
-    IV <- (r * IH) / (a * b * (1 - X))
-    SV <- (g * IV) / (a * c * X * exp(-g*EIP))
-    lambda <- SV * ((a * c * X) + g)
-    return(c("SV"=SV,"IV"=IV,"lambda"=lambda))
-  })
-}
-
-eq <- calc_equilibrium(parameters = params,SH = 500,IH = 100)
-pars1 <- params
-pars1["lambda"] <- eq["lambda"]
 
 out <- pfsim_aggregated(
   tmax = tmax,
-  SH = SH0,
-  IH = IH0,
-  SV = floor(eq["SV"]),
-  IV = floor(eq["IV"]),
-  parameters = pars1,
+  SH = IC$y0[["SH"]],
+  IH = IC$y0[["IH"]],
+  SV = IC$y0[["SV"]],
+  IV = IC$y0[["IV"]],
+  parameters = IC$parameters,
   verbose = T
 )
 
 out <- data.table::as.data.table(discretise(out = out,dt = 1))
 out <- data.table::melt(out,id.vars = "time",measure.vars = c("SH","IH","SV","IV"))
+out[, ("mean") := cumsum(value)/1:.N, by = .(variable)]
 
-ggplot(data = out) +
-  geom_line(aes(x=time,y=value,color=variable)) +
-  theme_bw()
+# ggplot(data = out) +
+#   geom_line(aes(x=time,y=value,color=variable)) +
+#   theme_bw()
 
-library(deSolve)
-
-
-derivs <- function(t,y,pars) {
-  
-  with(as.list(pars),{
-    SH <- y[1]
-    IH <- y[2]
-    SV <- y[3]
-    IV <- y[4]
-    
-    if(t <= EIP){
-      SV_eip <- SV0
-      
-      SH_eip <- SH0
-      IH_eip <- IH0
-    } else {
-      SV_eip <- lagvalue(t - EIP,3)
-      
-      SH_eip <- lagvalue(t - EIP,1)
-      IH_eip <- lagvalue(t - EIP,2)
-    }
-    
-    if(t <= LEP){
-      SH_lep <- SH0
-      IH_lep <- IH0
-      
-      IV_lep <- IV0
-    } else {
-      SH_lep <- lagvalue(t - LEP,1)
-      IH_lep <- lagvalue(t - LEP,2)
-      
-      IV_lep <- lagvalue(t - LEP,4)
-    }
-    
-    X_lep <- IH_lep / (SH_lep + IH_lep)
-    X_eip <- IH_eip / (SH_eip + IH_eip)
-    X <- IH / (SH + IH)
-    
-    dSH <- (r * IH) - (a * b * (1 - X) * IV)
-    dIH <- (a * b * IV_lep * (1 - X_lep)) - (r * IH)
-    
-    dSV <- lambda - (a * c * X * SV) - (g * SV)
-    dIV <- (a * c * X_eip * SV_eip * exp(-g * EIP)) - (g * IV)
-    
-    return(list(c(dSH,dIH,dSV,dIV)))
-  })
-}
-
-
-desolve_pars <- pars1
-desolve_pars[["SH0"]] <- SH0
-desolve_pars[["IH0"]] <- IH0
-desolve_pars[["SV0"]] <- eq[["SV"]]
-desolve_pars[["IV0"]] <- eq[["IV"]]
-
-y0 <- c("SH"=SH0,"IH"=IH0,"SV"=eq[["SV"]],"IV"=eq[["IV"]])
-
-dde_out <- dede(y = y0,times = 0:tmax,func = derivs,parms = desolve_pars)
-plot(dde_out)
+dde_out <- dede(y = IC$y0,times = 0:tmax,func = derivs,parms = IC$parameters)
 
 dde_out <- as.data.table(dde_out)
 dde_out <- data.table::melt(dde_out,id.vars = "time",measure.vars = c("SH","IH","SV","IV"))
 
-out[, ("type") := "stochastic"]
-dde_out[, ("type") := "dde"]
-
-ggplot(data = rbind(out,dde_out)) +
-  geom_line(aes(x=time,y=value,color=variable,linetype=type),alpha=0.85) +
+ggplot(data = out) +
+  geom_line(aes(x=time,y=value,color=variable),alpha=0.75) +
+  geom_line(aes(x=time,y=value,color=variable),alpha=0.75,data=dde_out,linetype=2) +
+  geom_line(aes(x=time,y=mean,color=variable)) +
   theme_bw()
 
-
-
-IHf <- dde_out[variable=="IH" & time == max(time),"value"][[1]]
-SHf <- dde_out[variable=="SH" & time == 18250,"value"][[1]]
-SVf <- dde_out[variable=="SV" & time == 18250,"value"][[1]]
-IVf <- dde_out[variable=="IV" & time == 18250,"value"][[1]]
-Xf <- (IHf / (IHf + SHf))
-
-# ok for IV
-
-(desolve_pars[["r"]]*IH0) 
-
-(desolve_pars[["r"]]*IHf) / (desolve_pars[["a"]] * desolve_pars[["b"]]  * (1 - Xf))
-
-# SV
-(desolve_pars[["g"]] * IVf) / (desolve_pars[["a"]] * desolve_pars[["c"]] * Xf * exp(-desolve_pars[["g"]] * desolve_pars[["EIP"]]))
-
-# lambda
-SVf * ((desolve_pars[["a"]] * desolve_pars[["c"]] * Xf) + desolve_pars[["g"]])
-
-
-
-
-
-
-# try again
-g <- params[["g"]]
-b <- params[["b"]]
-c <- params[["c"]]
-a <- params[["a"]]
-r <- params[["r"]]
-EIP <- params[["EIP"]]
-LEP <- params[["LEP"]]
-P <- exp(-g*EIP)
-
-(r*IH0) / (a * b * (SH0 / (SH0 + IH0)))
