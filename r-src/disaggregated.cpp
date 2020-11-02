ff8a187884e366a25da204503885fe6c67f52921
slwu89 <slwu89@berkeley.edu>
2020-11-02 14:09:29 -0800

    progress on mosy traces

--- r-src/disaggregated.cpp
+++ r-src/disaggregated.cpp
@@ -12,6 +12,7 @@
 #include <array>
 #include <string>
 #include <limits>
+#include <tuple>
 #include <memory>
 #include <queue>
 
@@ -19,18 +20,29 @@
 
 const static double infinity = std::numeric_limits<double>::infinity();
 
+// data structure
 using queue = std::priority_queue<double,std::vector<double>,std::greater<double> >;
 
-enum class intensity_type {SV,IV,X};
+// data structure to pass traces
+using queue_tuple = std::tuple<int,double>;
+auto queue_comp = [](const queue_tuple& a, const queue_tuple& b ) -> bool { return std::get<1>(a) > std::get<1>(b); };
+using queue_trace = std::priority_queue<queue_tuple,std::vector<queue_tuple>, decltype(queue_comp) >;
 
-// if have these, plus the end time of the step, can calculate the integrated intensity
-typedef struct integrated_intensity {
+// [[Rcpp::export]]
+void testqueue(){
+  queue_trace sample(queue_comp);
+  sample.push(queue_tuple(1,R::rlnorm(0.,1.)));
+  sample.push(queue_tuple(2,R::rlnorm(0.,1.)));
+  sample.push(queue_tuple(3,R::rlnorm(0.,1.)));
+  sample.push(queue_tuple(4,infinity));
+  while(!sample.empty()){
+    queue_tuple n = sample.top();
+    sample.pop();
+    Rcpp::Rcout <<  " int val: " << std::get<0>(n) << " double val: " << std::get<1>(n) << " --- \n";
+  }
+};
 
-  double t0;
-  double val;
-  intensity_type type;
 
-} integrated_intensity;
 
 // mosquito population
 typedef struct mosypop_str {
@@ -60,13 +72,12 @@ typedef struct mosypop_str {
   std::array<double,3> Tk{0.};
   std::array<double,3> ak{0.};
 
-  // integrated susceptible (Sv) biting
-  // integrated infectious (Iv) biting
-  double Sv_bite;
-  double Iv_bite;
+  // integrated susceptible (Sv) biting over [t0,t0+dt)
+  // integrated infectious (Iv) biting over [t0,t0+dt)
+  queue_trace Sv_trace;
+  queue_trace Iv_trace;
 
-  // mosypop_str(){};
-  // ~mosypop_str(){};
+  mosypop_str() : Sv_trace(queue_comp), Iv_trace(queue_comp) {};
 
 } mosypop_str;
 
@@ -106,13 +117,15 @@ mosypop_ptr make_mosypop(const Rcpp::NumericVector parameters, int SV, int IV){
 // iterate the mosquitoes
 void run_mosypop(mosypop_ptr& mosypop, double t0, double dt){
 
+  // push initial state into traces
+  if(!mosypop->Sv_trace.empty() || !mosypop->Iv_trace.empty()){
+    Rcpp::stop("'Sv_trace' and 'Iv_trace' should always be empty at start of time step \n");
+  }
+  mosypop->Sv_trace.push(queue_tuple(mosypop->S,t0));
+  mosypop->Iv_trace.push(queue_tuple(mosypop->I,t0));
+
   double tmax{t0+dt};
   double P = exp(-mosypop->g*mosypop->EIP);
-
-  // integrated susceptible (Sv) biting
-  // integrated infectious (Iv) biting
-  mosypop->Sv_bite = 0.;
-  mosypop->Iv_bite = 0.;
 
   // get bites from the previous time step
   while(mosypop->H2M_bites.top() != infinity){
@@ -139,44 +152,44 @@ void run_mosypop(mosypop_ptr& mosypop, double t0, double dt){
     // update putative time
     double delta = mosypop->delta_t[mu];
     double tsamp = mosypop->tnow + delta;
-    // if overrun, advance time to tmax and adjust the Poisson processes
+    // if overrun, advance time to tmax and adjust the Poisson processes for the fast-forward
     if(tsamp > tmax){
       double remaining = tmax - mosypop->tnow;
       for(int j=0; j<3; j++){
         mosypop->Tk[j] += mosypop->ak[j] * remaining;
       }
-      mosypop->Sv_bite += mosypop->a * mosypop->S * remaining;
-      mosypop->Iv_bite += mosypop->a * mosypop->I * remaining;
+      mosypop->Sv_trace.push(queue_tuple(mosypop->S,tmax));
+      mosypop->Iv_trace.push(queue_tuple(mosypop->I,tmax));
       mosypop->tnow = tmax;
       break;
     }
-
-    // update time after updating integrated biting
-    mosypop->Sv_bite += mosypop->a * mosypop->S * delta;
-    mosypop->Iv_bite += mosypop->a * mosypop->I * delta;
     mosypop->tnow = tsamp;
 
     // update system
     if(mu == 1){
       mosypop->S += 1;
+      mosypop->Sv_trace.push(queue_tuple(mosypop->S,mosypop->tnow));
     } else if(mu == 2){
       mosypop->S -= 1;
+      mosypop->Sv_trace.push(queue_tuple(mosypop->S,mosypop->tnow));
     } else if(mu == 3){
       mosypop->I -= 1;
+      mosypop->Iv_trace.push(queue_tuple(mosypop->I,mosypop->tnow));
     } else if(mu == 4){
       mosypop->M_inf.pop();
       mosypop->I += 1;
+      mosypop->Iv_trace.push(queue_tuple(mosypop->I,mosypop->tnow));
     } else {
       std::string msg("invalid minimum element in 'run_mosypop': " + std::to_string(mu));
       Rcpp::stop(msg);
     }
 
-
+    // update Tk
 
 
   }
 
-  // drawk internal firing times
+  // draw internal firing times
 
 
 }
