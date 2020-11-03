05169814cf1a6914879ee6bba4f656a1668dbbce
slwu89 <slwu89@berkeley.edu>
2020-11-03 11:46:55 -0800

    human model not working correctly

--- r-src/disaggregated.cpp
+++ r-src/disaggregated.cpp
@@ -30,7 +30,7 @@ const static double infinity = std::numeric_limits<double>::infinity();
 using queue = std::priority_queue<double,std::vector<double>,std::greater<double> >;
 
 // data structure to pass traces
-using queue_tuple = std::tuple<int,double>;
+using queue_tuple = std::tuple<double,double>; // 1st element is state, 2nd is time
 auto queue_comp = [](const queue_tuple& a, const queue_tuple& b ) -> bool { return std::get<1>(a) > std::get<1>(b); };
 using queue_trace = std::priority_queue<queue_tuple,std::vector<queue_tuple>, decltype(queue_comp) >;
 
@@ -230,6 +230,291 @@ void run_mosypop(mosypop_ptr& mosypop, double t0, double dt){
 }
 
 
+/* --------------------------------------------------------------------------------
+#   human population
+-------------------------------------------------------------------------------- */
+
+typedef struct humanpop_str {
+
+  // states
+  int S;
+  int I;
+  // double N;
+
+  // history tracking
+  std::vector<double> t_hist;
+  std::vector<double> S_hist;
+  std::vector<double> I_hist;
+
+  double tnow;
+
+  // bites when M->H transmission occured
+  queue M2H_bites;
+
+  // when the resulting infection in M will manifest
+  queue H_inf;
+
+  // parameters
+  double r;
+  double LEP;
+
+  // elements for MNRM of internal mosy dynamics
+  std::array<double,2> delta_t{infinity};
+  double Pk{0.};
+  double Tk{0.};
+  double ak{0.};
+
+  // integrated prevalence over [t0,t0+dt)
+  queue_trace X_trace;
+
+  humanpop_str() : X_trace(queue_comp) {};
+
+} humanpop_str;
+
+// use this pointer
+using humanpop_ptr = std::unique_ptr<humanpop_str>;
+
+// make the humans
+humanpop_ptr make_humanpop(const Rcpp::NumericVector parameters, const int SH, const int IH, const int outsize){
+  humanpop_ptr humanpop = std::make_unique<humanpop_str>();
+
+  humanpop->S = SH;
+  humanpop->I = IH;
+  // humanpop->N = SH+IH;
+
+  humanpop->t_hist.reserve(outsize);
+  humanpop->S_hist.reserve(outsize);
+  humanpop->I_hist.reserve(outsize);
+
+  humanpop->tnow = 0.0;
+
+  humanpop->H_inf.push(infinity);
+
+  humanpop->r = parameters["r"];
+  humanpop->LEP = parameters["LEP"];
+
+  // calculate initial propensities
+  humanpop->ak = humanpop->r * static_cast<double>(humanpop->I);
+
+  // draw initial firing times
+  humanpop->Pk = log(1. / R::runif(0.,1.));
+
+  return humanpop;
+};
+
+// iterate the humans
+void run_humanpop(humanpop_ptr& humanpop, double t0, double dt){
+
+  // push initial state into traces
+  if(!humanpop->X_trace.empty()){
+    Rcpp::stop("'X_trace' should always be empty at start of time step \n");
+  }
+
+  double tmax{t0+dt};
+
+  double X = static_cast<double>(humanpop->I) / (static_cast<double>(humanpop->S) + static_cast<double>(humanpop->I));
+  humanpop->X_trace.push(queue_tuple(X,t0));
+
+  // get bites from the previous time step
+  while(!humanpop->M2H_bites.empty()){
+    // take out one S person (they move to E)
+    humanpop->S -= 1;
+    // push the future infection
+    humanpop->H_inf.push(humanpop->M2H_bites.top() + humanpop->LEP);
+    humanpop->M2H_bites.pop();
+  }
+
+  // simulate dynamics over this time step
+  while(humanpop->tnow < tmax){
+
+    // absolute next firing times
+    humanpop->delta_t[0] = (humanpop->Pk - humanpop->Tk) / humanpop->ak;
+    humanpop->delta_t[1] = humanpop->H_inf.top() - humanpop->tnow;
+
+    // find minimum
+    int mu{1};
+    if(humanpop->delta_t[0] < humanpop->delta_t[1]){
+      mu = 0;
+    }
+
+    // update putative time
+    double delta = humanpop->delta_t[mu];
+    double tsamp = humanpop->tnow + delta;
+    // if overrun, advance time to tmax and adjust the Poisson process for the fast forward
+    if(tsamp > tmax){
+      double remaining = tmax - humanpop->tnow;
+      humanpop->Tk += humanpop->ak * remaining;
+      X = static_cast<double>(humanpop->I) / (static_cast<double>(humanpop->S) + static_cast<double>(humanpop->I));
+      humanpop->X_trace.push(queue_tuple(X,tmax));
+      humanpop->tnow = tmax;
+      break;
+    }
+    humanpop->tnow = tsamp;
+
+    // update system
+    if(mu == 0){
+      humanpop->I -= 1;
+      humanpop->S += 1;
+    } else if(mu == 1){
+      humanpop->H_inf.pop();
+      humanpop->I += 1;
+    } else {
+      std::string msg("invalid minimum element in 'run_mosypop': " + std::to_string(mu));
+      Rcpp::stop(msg);
+    }
+
+    X = static_cast<double>(humanpop->I) / (static_cast<double>(humanpop->S) + static_cast<double>(humanpop->I));
+    humanpop->X_trace.push(queue_tuple(X,humanpop->tnow));
+
+    // update Tk
+    humanpop->Tk += humanpop->ak * delta;
+
+    // update Pk
+    if(mu == 0){
+      humanpop->Pk += log(1. / R::runif(0.,1.));
+    }
+
+    // recalculate propensity
+    humanpop->ak = humanpop->r * static_cast<double>(humanpop->I);
+
+    // track output
+    humanpop->t_hist.push_back(humanpop->tnow);
+    humanpop->S_hist.push_back(humanpop->S);
+    humanpop->I_hist.push_back(humanpop->I);
+
+  }
+
+}
+
+
+/* --------------------------------------------------------------------------------
+#   bloodmeal
+-------------------------------------------------------------------------------- */
+
+void bloodmeal(const Rcpp::NumericVector parameters, humanpop_ptr& humanpop, mosypop_ptr& mosypop){
+
+  double a = parameters["a"];
+  double c = parameters["c"];
+  double b = parameters["b"];
+
+  // H2M bites occur with intensity (acX)S_{V}
+  // M2H bites occur with intensity ab(1-X)I_{V}
+  double H2M_intensity, M2H_intensity;
+  int H2M_bites, M2H_bites;
+
+  // length of intervals
+  double t1,t0,dt;
+  double S,I,X;
+
+  // next state change
+  std::array<double,3> t1_array{0.};
+
+  // beginning of piecewise trajectories
+  queue_tuple t0_SV = mosypop->Sv_trace.top();
+  queue_tuple t0_IV = mosypop->Iv_trace.top();
+  queue_tuple t0_X = humanpop->X_trace.top();
+
+  mosypop->Sv_trace.pop();
+  mosypop->Iv_trace.pop();
+  humanpop->X_trace.pop();
+
+  t0 = std::get<1>(t0_SV);
+
+  // end of the piecewise trajectories
+  queue_tuple t1_SV;
+  queue_tuple t1_IV;
+  queue_tuple t1_X;
+
+  // compute over the TWICE step
+  while(!mosypop->Sv_trace.empty() || !mosypop->Iv_trace.empty() || !humanpop->X_trace.empty()){
+
+    H2M_intensity = 0.;
+    M2H_intensity = 0.;
+
+    // if not empty, get the next state change in each trajectory
+    std::fill(t1_array.begin(),t1_array.end(),infinity);
+    if(!mosypop->Sv_trace.empty()){
+      t1_SV = mosypop->Sv_trace.top();
+      mosypop->Sv_trace.pop();
+      t1_array[0] = std::get<1>(t1_SV);
+    }
+    if(!mosypop->Iv_trace.empty()){
+      t1_IV = mosypop->Iv_trace.top();
+      mosypop->Iv_trace.pop();
+      t1_array[1] = std::get<1>(t1_IV);
+    }
+    if(!humanpop->X_trace.empty()){
+      t1_X = humanpop->X_trace.top();
+      humanpop->X_trace.pop();
+      t1_array[2] = std::get<1>(t1_X);
+    }
+
+    // find which trajectory changes next
+    auto min_elem = std::min_element(t1_array.begin(), t1_array.end());
+    int mu = std::distance(t1_array.begin(), min_elem);
+
+    // compute length of this piecewise interval [t0,d1)
+    t1 = t1_array[mu];
+    dt = t1 - t0;
+
+    // state values at t0
+    S = std::get<0>(t0_SV);
+    I = std::get<0>(t0_IV);
+    X = std::get<0>(t0_X);
+
+    // intensity over the interval
+    H2M_intensity = a * c * X * S * dt;
+    M2H_intensity = a * b * (1. - X) * I * dt;
+
+    // sample values
+    H2M_bites = R::rpois(H2M_intensity);
+    M2H_bites = R::rpois(M2H_intensity);
+
+    // add the bites to the mosquito for next time
+    if(H2M_bites > 0){
+      double btime = R::runif(t0,t1);
+      mosypop->H2M_bites.push(btime);
+    }
+
+    // add the bites to the human for next time
+    if(M2H_bites > 0){
+      double btime = R::runif(t0,t1);
+      humanpop->M2H_bites.push(btime);
+    }
+
+    // state change that happened first becomes new starting point
+    if(mu==0){
+      t0_SV = t1_SV;
+    } else if(mu==1){
+      t0_IV = t1_IV;
+    } else if(mu==2){
+      t0_X = t1_X;
+    } else{
+      std::string msg("invalid minimum element in 'bloodmeal': " + std::to_string(mu));
+      Rcpp::stop(msg);
+    }
+
+    // last end time becomes next beginning time
+    t0 = t1;
+
+  }
+
+};
+
+
+
+
+
+
+
+
+
+
+
+/* --------------------------------------------------------------------------------
+#   run (main)
+-------------------------------------------------------------------------------- */
+
 // [[Rcpp::export]]
 Rcpp::NumericMatrix test_mosquitoes(const Rcpp::NumericVector parameters, const double X, const int SV, const int IV, const double dt, const double tmax){
 
@@ -239,8 +524,6 @@ Rcpp::NumericMatrix test_mosquitoes(const Rcpp::NumericVector parameters, const 
   // some parameters
   double a = parameters["a"];
   double c = parameters["c"];
-
-  std::vector<double> bloodmeals;
 
   // 1st iteration
   double clock{0.0};
@@ -252,7 +535,7 @@ Rcpp::NumericMatrix test_mosquitoes(const Rcpp::NumericVector parameters, const 
     run_mosypop(mosypop, clock, dt);
 
     // compute bloodmeal times (the BLOODMEAL MODULE)
-    bloodmeals.clear();
+
     // start of this piecewise trajectory
     queue_tuple t0_block = mosypop->Sv_trace.top();
     mosypop->Sv_trace.pop();
@@ -273,7 +556,6 @@ Rcpp::NumericMatrix test_mosquitoes(const Rcpp::NumericVector parameters, const 
 
       // intensity for this piecewise block
       double intensity = a * c * X * S * dt;
-      // ddt+=dt;
       int bites = R::rpois(intensity);
 
       // add the bites to the mosquito for next time
@@ -308,42 +590,71 @@ Rcpp::NumericMatrix test_mosquitoes(const Rcpp::NumericVector parameters, const 
 };
 
 
-/* --------------------------------------------------------------------------------
-#   human population
--------------------------------------------------------------------------------- */
+// [[Rcpp::export]]
+Rcpp::NumericMatrix test_humans(const Rcpp::NumericVector parameters, const int SH, const int IH, const double IV, const double dt, const double tmax){
 
-typedef struct humanpop_str {
+  const int outsize = 1E5;
+  humanpop_ptr humanpop = make_humanpop(parameters,SH,IH,outsize);
 
-  // states
-  int S;
-  int I;
+  // some parameters
+  double a = parameters["a"];
+  double b = parameters["b"];
 
-  // history tracking
-  std::vector<double> t_hist;
-  std::vector<double> S_hist;
-  std::vector<double> I_hist;
+  // 1st iteration
+  double clock{0.0};
 
-  double tnow;
+  // subsequent iteration
+  while(clock < tmax){
 
-  // bites when M->H transmission occured
-  queue M2H_bites;
+    // iterate the mosquitoes
+    run_humanpop(humanpop, clock, dt);
 
-  // when the resulting infection in M will manifest
-  queue H_inf;
+    // compute bloodmeal times (the BLOODMEAL MODULE)
 
-  // parameters
-  double r;
-  double LEP;
+    // start of this piecewise trajectory
+    queue_tuple t0_block = humanpop->X_trace.top();
+    humanpop->X_trace.pop();
+    // end of this piecewise trajectory
+    queue_tuple t1_block;
+    // iterate over parts
+    // double ddt{0.};
+    while(!humanpop->X_trace.empty()){
+      // grab the end
+      t1_block = humanpop->X_trace.top();
+      humanpop->X_trace.pop();
 
-  // elements for MNRM of internal mosy dynamics
-  std::array<double,2> delta_t{infinity};
-  double Pk{0.};
-  double Tk{0.};
-  double ak{0.};
+      // dt: length of trajectory, S: the susceptible mosy over this traj
+      double t1 = std::get<1>(t1_block);
+      double t0 = std::get<1>(t0_block);
+      double dt = t1 - t0;
+      double X = std::get<0>(t0_block);
+      // Rcpp::Rcout <<"X: " << X << "---\n";
 
-  // integrated prevalence over [t0,t0+dt)
-  queue_trace X_trace;
+      // intensity for this piecewise block
+      double intensity = a * b * (1. - X) * IV * dt;
+      int bites = R::rpois(intensity);
 
-  humanpop_str() : X_trace(queue_comp) {};
+      // add the bites to the mosquito for next time
+      if(bites > 0){
+        double btime = R::runif(t0,t1);
+        humanpop->M2H_bites.push(btime);
+      }
 
-} humanpop_str;
+      // set endpoint to startpoint for next iteration
+      t0_block = t1_block;
+    }
+
+    // increment tmax before next iteration
+    clock += dt;
+  }
+
+  int k = humanpop->t_hist.size();
+  Rcpp::NumericMatrix out(k,3);
+  Rcpp::colnames(out) = Rcpp::CharacterVector::create("time","S","I");
+  for(int j=0; j<k; j++){
+    out(j,0) = humanpop->t_hist[j];
+    out(j,1) = humanpop->S_hist[j];
+    out(j,2) = humanpop->I_hist[j];
+  }
+  return out;
+};
--- scripts/test-disaggregated.R
+++ scripts/test-disaggregated.R
@@ -15,13 +15,29 @@ NH <- 1e3
 X <- 0.3
 
 IC <- calc_equilibrium(NH = NH,X = X,EIP = 11)
+IC$y0
 
-tmax <- 365*5
+tmax <- 365*100
 
 mout <- test_mosquitoes(parameters = IC$parameters,X = X,SV = IC$y0[["SV"]],IV = IC$y0[["IV"]],dt = 5,tmax = tmax)
+mout <- as.data.table(stocheulerABM::discretise(out = mout,dt = 1))
+mout <- data.table::melt(data = mout,id.vars="time")
 
-matplot(mout[,-1],type="l")
-IC$parameters[["lambda"]]/IC$parameters[["g"]]
-mean(mout[,2])
-mean(mout[,3])
-mean(mout[,2])+mean(mout[,3])
+ggplot(data = mout) +
+  geom_line(aes(x=time,y=value,color=variable)) +
+  theme_bw()
+
+mout[variable == "S", mean(value)]
+mout[variable == "I", mean(value)]
+
+
+hout <- test_humans(parameters = IC$parameters,SH = IC$y0[["SH"]],IH = IC$y0[["IH"]],IV = IC$y0[["IV"]],dt = 2,tmax = tmax)
+hout <- as.data.table(stocheulerABM::discretise(out = hout,dt = 1))
+hout <- data.table::melt(data = hout,id.vars="time")
+
+ggplot(data = hout) +
+  geom_line(aes(x=time,y=value,color=variable)) +
+  theme_bw()
+
+hout[variable == "S", mean(value)]
+hout[variable == "I", mean(value)]
